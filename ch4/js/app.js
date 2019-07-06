0fcba1c597c59358e9dd46b53f4e0babfc20fe16
Jeremy Yandell <jereyand@gmail.com>
2019-07-05 21:32:31 -0700

    ch4 question 1

--- ch4/js/app.js
+++ ch4/js/app.js
@@ -1,1 +1,29 @@
 console.log('working');
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+// As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+
+//--------------->Not working for bonus yet!<-------------------------------
+const range = (start, end, step = 1) =>  {
+    const rangeArr = [];
+    if (step > 0)  {
+    for (let i = start; i <= end; i += step)  {
+        rangeArr.push(i);
+        }
+    }
+    else {
+        for (let i = start; i >= end; i += step)  {
+        rangeArr.push(i);
+        }
+    } 
+    return rangeArr;
+};
+const sum = (someArray) =>  {
+    let sum = 0;
+    for (let i = 0; i < someArray.length; i++)  {
+        sum += someArray[i];
+    }
+    return sum;
+};
